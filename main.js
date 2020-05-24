47b80caed061fca272bc147c2c1cca030db6a96f
bsao17 <declic62@gmail.com>
2020-05-24 14:53:36 +0200

    Test style de la page et action jquery

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-FB.ui({
-    method: 'share',
-    href: 'https://developers.facebook.com/docs/'
-  }, function(response){});
+let displayRequest = document.getElementById('displayApi');
+
+
+
+function hello(){
+  let doc = document.createElement('div');
+  doc.innerHTML = '<a href="https://jquery.com/">jquery applicarion</a>'
+  displayRequest.appendChild(doc)
+
+}
+
+$('#bouton').on('click', hello);
